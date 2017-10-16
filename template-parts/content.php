f8c24b2435598dc217c45faa5ae22b0280308f07
markozdecuzcuz <gaudianomark@gmail.com>
2017-10-16 20:39:42 +0800

    content

--- template-parts/content.php
+++ template-parts/content.php
@@ -12,19 +12,34 @@
 				<div class="row">
 					<div class="col-md-12 first-con">
 						<h2>We take sales talent recruitment seriously</h2>
-					</div>
-					<div class="col-md-12 first-con">
 						<p>Our approach is to find the right people for the right companies every time. We do it by digging deeper and building relationships that truly drive both our partner's and candidate's futures forward.</p>
 					</div>
+					
 				</div>
-			</div>
-			<div class="container home-con">
-				<div class="row">
-					<div class="col-md-6">
-						<h2>We take sales talent recruitment seriously</h2>
-					</div>
-					<div class="col-md-6">
-						<p>Our approach is to find the right people for the right companies every time. We do it by digging deeper and building relationships that truly drive both our partner's and candidate's futures forward.</p>
+				</div>
+				<div class="container col-md-12 second-content">
+					<div class="row">
+						<div class="col-md-6 first-col">
+							<h2>CLIENTS</h2>
+							<p>Matching talented individuals with talent companies takes work. Our proven process makes that work go away.</p>
+							<a href="" class="btn">FIND OUT MORE</a>
+						</div>
+						<div class="col-md-6 second-col">
+							<h2>CANDIDATES</h2>
+							<p>Next steps aren't always simple to. So we go further to understand where you want to be and how we can help you there, simply.</p>
+							<a href="" class="btn">FIND OUT MORE</a>
+						</div>
 					</div>
 				</div>
-			</div>
+				<div class="container col-md-12 third-content">
+					<div class="row">
+						<div class="col-md-12 third-col">
+							<h2>Currently recruiting for</h2>
+							<p>Here a just few of the job roles we are currently recruiting for.</p>
+						</div>
+						<div class="">
+							<input type="haha" name="hah">
+						</div>	
+					</div>
+				</div>
+				
