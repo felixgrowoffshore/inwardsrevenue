c73841b520a8fafb7595f2572f3198123e7ace07
markozdecuzcuz <gaudianomark@gmail.com>
2017-10-16 19:22:48 +0800

    content

--- page-home.php
+++ page-home.php
@@ -9,33 +9,8 @@
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<div class="home-feat-wrap">
-				<?php get_template_part( 'template-parts/' ); ?>
+				<?php get_template_part( 'template-parts/content' ); ?>
 			</div>
-			<div class="container home-con">
-				<div class="row">
-					<div class="col-md-12 first-con">
-						<h2>We take sales talent recruitment seriously</h2>
-					</div>
-					<div class="col-md-12 first-con">
-						<p>Our approach is to find the right people for the right companies every time. We do it by digging deeper and building relationships that truly drive both our partner's and candidate's futures forward.</p>
-					</div>
-				</div>
-			</div>
-			<div class="container home-con">
-				<div class="row">
-					<div class="col-md-6">
-						<h2>We take sales talent recruitment seriously</h2>
-					</div>
-					<div class="col-md-6">
-						<p>Our approach is to find the right people for the right companies every time. We do it by digging deeper and building relationships that truly drive both our partner's and candidate's futures forward.</p>
-					</div>
-				</div>
-			</div>
-
-			
-
-
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
