2581c50ad0e4f7b466570f83b94422d4f1c5cb77
growoffs <f.bongo@growoffshore.com>
2017-10-17 16:18:18 +0800

    add content in featured content

--- template-parts/_featured_image.php
+++ template-parts/_featured_image.php
@@ -1,13 +1,20 @@
 <?php if ( has_post_thumbnail() ) :
   $thumbnail_id = get_post_thumbnail_id( get_the_ID() );
-  $thumb_img = get_post( $thumbnail_id );
+  //$thumb_img = get_post( $thumbnail_id );
   $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
-  $description = $thumb_img->post_content; ?>
+  //$description = $thumb_img->post_content; ?>
     <div class="featured_image">
       <img src="<?php the_post_thumbnail_url('full'); ?>" alt="<?php echo $alt; ?>" />
       <div class="feat-info">
-        <h1><?= the_title() ?></h1>
-        <p class=""><?php echo $description; ?></p>
+        <h1><?= the_title(); ?></h1>
+        <?php
+        // TO SHOW THE PAGE CONTENTS
+        while ( have_posts() ) : the_post(); ?>
+          <?php if( '' !== get_post()->post_content ) : ?>
+            <?=  the_content(); ?>
+          <?php endif;
+        endwhile; ?>
+
       </div>
       <?php if(is_front_page()) : ?>
         <p class="btn-next"><a class="next-block" href="#next-block"><i class="fa fa-long-arrow-down"></i></a></p>
