abb85f479652380858247314780411474a04a522
markozdecuzcuz <gaudianomark@gmail.com>
2017-10-17 14:35:31 +0800

    content page

--- template-parts/content.php
+++ template-parts/content.php
@@ -19,12 +19,12 @@
 				</div>
 				<div class="container col-md-12 second-content">
 					<div class="row">
-						<div class="col-md-6 first-col">
+						<div class="col-md-6 col-sm-6 col-xs-12 first-col">
 							<h2>CLIENTS</h2>
 							<p>Matching talented individuals with talent companies takes work. Our proven process makes that work go away.</p>
 							<a href="" class="btn">FIND OUT MORE</a>
 						</div>
-						<div class="col-md-6 second-col">
+						<div class="col-md-6 col-sm-6 col-xs-12 second-col">
 							<h2>CANDIDATES</h2>
 							<p>Next steps aren't always simple to. So we go further to understand where you want to be and how we can help you there, simply.</p>
 							<a href="" class="btn">FIND OUT MORE</a>
@@ -44,7 +44,7 @@
 										<input class="col-md-5" type="input" name="input" placeholder="KEYWORDS">
 										<input class="col-md-5" type="input" name="input" placeholder="LOCATION">
 										<a href="" class="btn col-md-2">SEARCH</a>	
-									<div class="filter">
+									<div id="filter">
 										<p class="col-md-2">Filter by tag</p>
 										<a href="" class="btn col-md-1 tag">tag title</a>
 										<a href="" class="btn col-md-1 tag">tag title</a>
@@ -57,6 +57,61 @@
 						</div>	
 					</div>
 				</div>
+				<div class="container fifth-content">
+					<div class="row fifth-row">
+						<form>
+						  <div>
+						    <input type="checkbox" id="subscribeNews" name="subscribe" value="newsletter">
+						    <label for="subscribeNews">FREELANCE</label>
+						    <input type="checkbox" id="subscribeNews" name="subscribe" value="newsletter">
+						    <label for="subscribeNews">FULL TIME</label>
+						    <input type="checkbox" id="subscribeNews" name="subscribe" value="newsletter">
+						    <label for="subscribeNews">PART TIME</label>
+						    <input type="checkbox" id="subscribeNews" name="subscribe" value="newsletter">
+						    <label for="subscribeNews">TEMPORARY</label>
+						  </div>
 
-			</div>
+						</form>
+					</div>
+	
+				</div>
+				<div class="container sixth-content">
+					<div class="row sixth-row">
+						<div class="col-md-1">
+							<img src="http://localhost:84/RC/wp-content/uploads/2017/10/box.png">
+						</div>
+						<div class="col-md-5">
+							<h5>Job Title</h5>
+							<p>Lorem ipsum dolor sit amet.</p>
+						</div>
+						<div class="col-md-3 london">
+							<p>London</p>
+						</div>
+						<div class="col-md-3 view-job">
+							<a class="btn" href="">VIEW JOB</a>
+						</div>
+					</div>
+
+				</div>
+				<div class="container sixth-content">
+					<div class="row sixth-row">
+						<div class="col-md-1">
+							<img src="http://localhost:84/RC/wp-content/uploads/2017/10/box.png">
+						</div>
+						<div class="col-md-5">
+							<h5>Job Title</h5>
+							<p>Lorem ipsum dolor sit amet.</p>
+						</div>
+						<div class="col-md-3 london">
+							<p>London</p>
+						</div>
+						<div class="col-md-3 view-job">
+							<a class="btn" href="">VIEW JOB</a>
+						</div>
+					</div>
+
+				</div>
+
+
+		
 
