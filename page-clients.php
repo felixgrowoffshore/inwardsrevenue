2a7ea4ca35c366ab5962ac1f1ebe7f2bb70d23bc
markozdecuzcuz <gaudianomark@gmail.com>
2017-10-17 15:50:00 +0800

    client template and page

---
+++ page-clients.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Clients
+ */
+
+?>
+<?php get_header(); ?>
+	
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<div class="home-feat-wrap">
+				<?php get_template_part( 'template-parts/content','clients' ); ?>
+			</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+
+<?php
+//get_sidebar();
+get_footer();
+?>
---
+++ template-parts/content-clients.php
@@ -0,0 +1,11 @@
+<div class="container client-con">
+	<div class="row client-row">
+		<h2>Relationships build business.</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec convallis dolor, nec viverra est. Aliquam porttitor purus a dolor elementum convallis. Nulla non fermentum tortor. Nulla in ante in nulla pulvinar blandit.Quisque mollis odio non quam vulputate, nec convallis tortor suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed elementum, metus ut vehicula commodo, quam diam accumsan odio, vel efficitur sapien purus a augue. Sed vitae purus eros.</p>
+	</div>
+</div>
+<div class="container client-con1">
+	<div class="row">
+		<h2 class="col-md-1">1</h2>
+	</div>
+</div>
