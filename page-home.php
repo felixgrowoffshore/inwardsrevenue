490b2463753d59dc94bfeb451909f60ae6c6f6f8
growoffs <f.bongo@growoffshore.com>
2017-10-17 14:50:10 +0800

    separate home content

--- page-home.php
+++ page-home.php
@@ -9,7 +9,7 @@
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<div class="home-feat-wrap">
-				<?php get_template_part( 'template-parts/content' ); ?>
+				<?php get_template_part( 'template-parts/home', 'content' ); ?>
 			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
