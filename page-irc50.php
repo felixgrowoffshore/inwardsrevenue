47be4a2b77595564de9b72cba31e7422555f16ce
markozdecuzcuz <gaudianomark@gmail.com>
2017-10-19 18:32:34 +0800

    irc50 page and styles

---
+++ page-irc50.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: IRC50
+ */
+
+?>
+<?php get_header(); ?>
+	
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<div class="home-feat-wrap">
+			<?php get_template_part( 'template-parts/_featured_image' ); ?>
+				<?php get_template_part( 'template-parts/content','IRC50' ); ?>
+			</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+//get_sidebar();
+get_footer();
+?>
