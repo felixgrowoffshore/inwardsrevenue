dc3fa9d6182dced9e8f6d891716048d92586f164
growoffs <f.bongo@growoffshore.com>
2017-10-23 11:40:08 +0800

    clients animation

--- library/development/js/theme-scripts.js
+++ library/development/js/theme-scripts.js
@@ -23,36 +23,71 @@ jQuery(document).ready(function($){
   });
 
 
+  // get top positions and references to all blocks
+var pos = $(".client-con").map(function(){
+  var $this = $(this);
+  return {
+    el: $this,
+    top: $this.offset().top
+  };
+}).get();
+
+// var $result = $("#result-text");
+
+// Set document scrolling event handler
+$(document).on("scroll", function() {
+  var visible = [];
+  var scrollStart = $(this).scrollTop();
+  var scrollEnd = scrollStart+$(window).height();
+  var vis=[];
+  clients_animate();
+  for (var i=0, l=pos.length; i<l; i++) {
+    if (pos[i].top < scrollStart || pos[i].top > scrollEnd) {
+      // console.log(pos[i].el.attr("class") + "=> " + (pos[i].top > scrollEnd ? 'true' : 'end'));
+      continue;
+    }
+    // console.log(pos[i].el.attr("class") + "=>" + pos[i].top);
+    // vis.push(pos[i].el.attr("class"));
+    pos[i].el.removeClass('fade-away');
+  }
+  // $result.text("");
+  // $result.text(vis.join(", "));
+
+}).scroll();
+
+function clients_animate(){
+  $(".client-con").addClass('fade-away');
+}
 
 //Client Page JS
- $(window).scroll(function(){
-    $(".fade-1").css("opacity", 1 - $(window).scrollTop() / 1000);
-    $(".side-p1").css("opacity", 1 - $(window).scrollTop() / 1000);
-    $(".side-p1").animate({left:100, opacity:"show"}, 1000);
-  });
-
-  $(window).scroll(function(){
-    $(".fade-2").css("opacity", 1 - $(window).scrollTop() / 1350);
-    $(".side-p2").css("opacity", 1 - $(window).scrollTop() / 1350);
-    $(".side-p2").animate({left:100, opacity:"show"}, 1500);
-  });
-
-  $(window).scroll(function(){
-    $(".fade-3").css("opacity", 1 - $(window).scrollTop() / 1700);
-    $(".side-p3").css("opacity", 1 - $(window).scrollTop() / 1700);
-    $(".side-p3").animate({left:100, opacity:"show"}, 2000);
-  });
-
-  $(window).scroll(function(){
-    $(".fade-4").css("opacity", 1 - $(window).scrollTop() / 2150);
-    $(".side-p4").css("opacity", 1 - $(window).scrollTop() / 2150);
-    $(".side-p4").animate({left:100, opacity:"show"}, 2500);
-  });
-
-  $(window).scroll(function(){
-    $(".fade-5").css("opacity", 1 - $(window).scrollTop() / 3000);
-    $(".side-p5").css("opacity", 1 - $(window).scrollTop() / 3000);
-    $(".side-p5").animate({left:100, opacity:"show"}, 3000);
-  });
+ // $(window).scroll(function(){
+ //    $(".fade-1").css("opacity", 1 - $(window).scrollTop() / 1000);
+ //    $(".side-p1").css("opacity", 1 - $(window).scrollTop() / 1000);
+ //    $(".side-p1").animate({left:100, opacity:"show"}, 1000);
+ //  });
+ //
+ //  $(window).scroll(function(){
+ //    $(".fade-2").css("opacity", 1 - $(window).scrollTop() / 1350);
+ //    $(".side-p2").css("opacity", 1 - $(window).scrollTop() / 1350);
+ //    $(".side-p2").animate({left:100, opacity:"show"}, 1500);
+ //  });
+ //
+ //  $(window).scroll(function(){
+ //    $(".fade-3").css("opacity", 1 - $(window).scrollTop() / 1700);
+ //    $(".side-p3").css("opacity", 1 - $(window).scrollTop() / 1700);
+ //    $(".side-p3").animate({left:100, opacity:"show"}, 2000);
+ //  });
+ //
+ //  $(window).scroll(function(){
+ //    $(".fade-4").css("opacity", 1 - $(window).scrollTop() / 2150);
+ //    $(".side-p4").css("opacity", 1 - $(window).scrollTop() / 2150);
+ //    $(".side-p4").animate({left:100, opacity:"show"}, 2500);
+ //  });
+ //
+ //  $(window).scroll(function(){
+ //    $(".fade-5").css("opacity", 1 - $(window).scrollTop() / 3000);
+ //    $(".side-p5").css("opacity", 1 - $(window).scrollTop() / 3000);
+ //    $(".side-p5").animate({left:100, opacity:"show"}, 3000);
+ //  });
 
 });
--- template-parts/content-clients.php
+++ template-parts/content-clients.php
@@ -1,77 +1,77 @@
 
-<div class="col-md-12  client-con1">
+<div class="col-md-12 client-con">
 	<div class="container client-cont1">
-		<div class="fade-1">
+		<div class="client-fade">
 			<h3 class="col-md-1 col-sm-2 col-xs-3 number">1</h3>
 			<div class="col-md-2 col-sm-4 col-xs-9 scope">
 				<h3>SCOPE</h3>
 				<p>Talent isn't the some as experience.</p>
 			</div>
 		</div>
-		<div class="col-md-9 col-sm-12 col-xs-12 side-p1">
+		<div class="col-md-9 col-sm-12 col-xs-12 slide-right">
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec convallis dolor, nec viverra est. Aliquam porttitor purus a dolor elementum convallis. Nulla non fermentum tortor. Nulla in ante in nulla pulvinar blandit.</p>
 			<p>Quisque mollis odio non quam vulputate, nec convallis tortor suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed elementum, metus ut vehicula commodo, quam diam accumsan odio, vel efficitur sapien purus a augue. Sed vitae purus eros.</p>
-		</div>	
+		</div>
 	</div>
 </div>
-<div class="col-md-12  client-con2">
+<div class="col-md-12 client-con">
 	<div class="container client-cont2">
-		<div class="fade-2">
+		<div class="client-fade">
 			<h3 class="col-md-1 col-sm-2 col-xs-3 number">2</h3>
 			<div class="col-md-2 col-sm-4 col-xs-9 scope">
 				<h3 class="">SEARCH</h3>
 				<p>We know where to look</p>
 			</div>
 		</div>
-		<div class="col-md-9 col-sm-12 col-xs-12 side-p2">
+		<div class="col-md-9 col-sm-12 col-xs-12 slide-right">
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec convallis dolor, nec viverra est. Aliquam porttitor purus a dolor elementum convallis. Nulla non fermentum tortor. Nulla in ante in nulla pulvinar blandit.</p>
 			<p>Quisque mollis odio non quam vulputate, nec convallis tortor suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed elementum, metus ut vehicula commodo, quam diam accumsan odio, vel efficitur sapien purus a augue. Sed vitae purus eros.</p>
-		</div>	
+		</div>
 	</div>
 </div>
-<div class="col-md-12  client-con3">
+<div class="col-md-12 client-con">
 	<div class="container client-cont3">
-		<div class="fade-3">
+		<div class="client-fade">
 			<h3 class="col-md-1 col-sm-2 col-xs-3 number">3</h3>
 			<div class="col-md-2 col-sm-4 col-xs-9 scope">
 				<h3 class="">SELECT</h3>
 				<p>If we don't see the talent, why would you?</p>
 			</div>
 		</div>
-		<div class="col-md-9 col-sm-12 col-xs-12 side-p3">
+		<div class="col-md-9 col-sm-12 col-xs-12 slide-right">
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec convallis dolor, nec viverra est. Aliquam porttitor purus a dolor elementum convallis. Nulla non fermentum tortor. Nulla in ante in nulla pulvinar blandit.</p>
 			<p>Quisque mollis odio non quam vulputate, nec convallis tortor suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed elementum, metus ut vehicula commodo, quam diam accumsan odio, vel efficitur sapien purus a augue. Sed vitae purus eros.</p>
-		</div>	
+		</div>
 	</div>
 </div>
-<div class="col-md-12  client-con4">
+<div class="col-md-12 client-con">
 	<div class="container client-cont4">
-		<div class="fade-4">
+		<div class="client-fade">
 			<h3 class="col-md-1 col-sm-2 col-xs-3 number">4</h3>
 			<div class="col-md-2 col-sm-4 col-xs-9 scope">
 				<h3 class="">SPARK</h3>
 				<p>Recruitment doesn't end with the job offer.</p>
 			</div>
 		</div>
-		<div class="col-md-9 col-sm-12 col-xs-12 side-p4">
+		<div class="col-md-9 col-sm-12 col-xs-12 slide-right">
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec convallis dolor, nec viverra est. Aliquam porttitor purus a dolor elementum convallis. Nulla non fermentum tortor. Nulla in ante in nulla pulvinar blandit.</p>
 			<p>Quisque mollis odio non quam vulputate, nec convallis tortor suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed elementum, metus ut vehicula commodo, quam diam accumsan odio, vel efficitur sapien purus a augue. Sed vitae purus eros.</p>
-		</div>	
+		</div>
 	</div>
 </div>
-<div class="col-md-12  client-con5">
+<div class="col-md-12 client-con">
 	<div class="container client-cont5">
-		<div class="fade-5">
+		<div class="client-fade">
 			<h3 class="col-md-1 col-sm-2 col-xs-3 number">5</h3>
 			<div class="col-md-2 col-sm-4 col-xs-9 scope">
 				<h3 class="">SECURE</h3>
 				<p>In love yet?</p>
 			</div>
 		</div>
-		<div class="col-md-9 col-sm-12 col-xs-12 side-p5">
+		<div class="col-md-9 col-sm-12 col-xs-12 slide-right">
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec convallis dolor, nec viverra est. Aliquam porttitor purus a dolor elementum convallis. Nulla non fermentum tortor. Nulla in ante in nulla pulvinar blandit.</p>
 			<p>Quisque mollis odio non quam vulputate, nec convallis tortor suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed elementum, metus ut vehicula commodo, quam diam accumsan odio, vel efficitur sapien purus a augue. Sed vitae purus eros.</p>
-		</div>	
+		</div>
 	</div>
 </div>
 <div class="wrapper wrapper-6">
@@ -90,6 +90,3 @@
 	</div>
 </div>
 </div>
-
-
-
