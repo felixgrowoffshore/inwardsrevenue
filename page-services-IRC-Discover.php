acb6956fc2c064a39a6021ef16060cf6abaf8f79
markozdecuzcuz <gaudianomark@gmail.com>
2017-10-23 18:57:15 +0800

    IRC ENGAGE and discover page

---
+++ page-services-IRC-Discover.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: IRC-Discover
+ */
+
+?>
+<?php get_header(); ?>
+	
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<div class="home-feat-wrap">
+			<?php get_template_part( 'template-parts/_featured_image' ); ?>
+			</div>
+			<div class="home-content">
+				<?php get_template_part( 'template-parts/ircdiscover', 'content' ); ?>
+			</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+//get_sidebar();
+get_footer();
+?>
