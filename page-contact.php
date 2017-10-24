b70cf7d89f00c1b47bbb3348945165a668f06256
growoffs <f.bongo@growoffshore.com>
2017-10-24 19:23:44 +0800

    remove unused classes

--- page-contact.php
+++ page-contact.php
@@ -9,7 +9,7 @@
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<div class="feat-wrap">
-				<?php get_template_part( 'template-parts/_featured_image' ); ?>
+				<?php get_template_part( 'template-parts/_featured_image_default' ); ?>
 			</div>
 			<div class="contact-content">
 				<?php get_template_part( 'template-parts/contact','content' ); ?>
--- page-services.php
+++ page-services.php
@@ -5,11 +5,11 @@
 
 ?>
 <?php get_header(); ?>
-	
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-			<div class="home-feat-wrap">
-			<?php get_template_part( 'template-parts/_featured_image' ); ?>
+			<div class="feat-wrap">
+			<?php get_template_part( 'template-parts/_featured_image_default' ); ?>
 			</div>
 			<div class="services-content">
 				<?php get_template_part( 'template-parts/services', 'content' ); ?>
