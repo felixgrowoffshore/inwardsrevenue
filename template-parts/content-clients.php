0472422dd9ef78a33f6649e778fbbeeeae08b392
growoffs <f.bongo@growoffshore.com>
2017-10-24 16:23:14 +0800

    make client template dynamic

--- template-parts/content-clients.php
+++ template-parts/content-clients.php
@@ -1,92 +1,31 @@
-
-<div class="client-con pink1 colours">
-	<div class="container client-cont1">
-		<div class="client-fade">
-			<h3 class="col-md-1 col-sm-2 col-xs-3 number">1</h3>
-			<div class="col-md-2 col-sm-4 col-xs-9 scope">
-				<h3>SCOPE</h3>
-				<p>Talent isn't the some as experience.</p>
+<?php
+	$client_contents = get_field('page_contents');
+	$find_more = get_field('find_out_more');
+	foreach ($client_contents as $key => $client_content) {
+		?>
+		<div class="client-con <?= $client_content['background_color'] ?> colours">
+			<div class="container">
+				<div class="client-fade">
+					<h3 class="col-md-1 col-sm-2 col-xs-3 number"><?= ($key + 1) ?></h3>
+					<div class="col-md-2 col-sm-4 col-xs-9 scope">
+						<h3><?= $client_content['name'] ?></h3>
+						<p><?= $client_content['caption'] ?></p>
+					</div>
+				</div>
+				<div class="col-md-9 col-sm-12 col-xs-12 slide-right">
+					<?= $client_content['description'] ?>
+				</div>
 			</div>
 		</div>
-		<div class="col-md-9 col-sm-12 col-xs-12 slide-right">
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec convallis dolor, nec viverra est. Aliquam porttitor purus a dolor elementum convallis. Nulla non fermentum tortor. Nulla in ante in nulla pulvinar blandit.</p>
-			<p>Quisque mollis odio non quam vulputate, nec convallis tortor suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed elementum, metus ut vehicula commodo, quam diam accumsan odio, vel efficitur sapien purus a augue. Sed vitae purus eros.</p>
-		</div>
-	</div>
-</div>
-<div class="pink2 colours client-con">
-	<div class="container client-cont2">
-		<div class="client-fade">
-			<h3 class="col-md-1 col-sm-2 col-xs-3 number">2</h3>
-			<div class="col-md-2 col-sm-4 col-xs-9 scope">
-				<h3 class="">SEARCH</h3>
-				<p>We know where to look</p>
+		<?php
+	}
+	foreach ($find_more as $key => $more) { ?>
+		<div class="wrapper wrapper-6 <?= $more['background_color'] ?> colours">
+			<div class="container client-con6">
+				<div class="row client-row6">
+					<h5 class="col-md-6 offset-md-3"><?= $more['info'] ?></h5>
+					<a class="btn col-md-3" href="<?= $more['link'] ?>">FIND OUT MORE</a>
+				</div>
 			</div>
 		</div>
-		<div class="col-md-9 col-sm-12 col-xs-12 slide-right">
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec convallis dolor, nec viverra est. Aliquam porttitor purus a dolor elementum convallis. Nulla non fermentum tortor. Nulla in ante in nulla pulvinar blandit.</p>
-			<p>Quisque mollis odio non quam vulputate, nec convallis tortor suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed elementum, metus ut vehicula commodo, quam diam accumsan odio, vel efficitur sapien purus a augue. Sed vitae purus eros.</p>
-		</div>
-	</div>
-</div>
-<div class="client-con pink3 colours">
-	<div class="container client-cont3">
-		<div class="client-fade">
-			<h3 class="col-md-1 col-sm-2 col-xs-3 number">3</h3>
-			<div class="col-md-2 col-sm-4 col-xs-9 scope">
-				<h3 class="">SELECT</h3>
-				<p>If we don't see the talent, why would you?</p>
-			</div>
-		</div>
-		<div class="col-md-9 col-sm-12 col-xs-12 slide-right">
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec convallis dolor, nec viverra est. Aliquam porttitor purus a dolor elementum convallis. Nulla non fermentum tortor. Nulla in ante in nulla pulvinar blandit.</p>
-			<p>Quisque mollis odio non quam vulputate, nec convallis tortor suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed elementum, metus ut vehicula commodo, quam diam accumsan odio, vel efficitur sapien purus a augue. Sed vitae purus eros.</p>
-		</div>
-	</div>
-</div>
-<div class="pink4 colours client-con">
-	<div class="container client-cont4">
-		<div class="client-fade">
-			<h3 class="col-md-1 col-sm-2 col-xs-3 number">4</h3>
-			<div class="col-md-2 col-sm-4 col-xs-9 scope">
-				<h3 class="">SPARK</h3>
-				<p>Recruitment doesn't end with the job offer.</p>
-			</div>
-		</div>
-		<div class="col-md-9 col-sm-12 col-xs-12 slide-right">
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec convallis dolor, nec viverra est. Aliquam porttitor purus a dolor elementum convallis. Nulla non fermentum tortor. Nulla in ante in nulla pulvinar blandit.</p>
-			<p>Quisque mollis odio non quam vulputate, nec convallis tortor suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed elementum, metus ut vehicula commodo, quam diam accumsan odio, vel efficitur sapien purus a augue. Sed vitae purus eros.</p>
-		</div>
-	</div>
-</div>
-<div class="pink5 colours client-con">
-	<div class="container client-cont5">
-		<div class="client-fade">
-			<h3 class="col-md-1 col-sm-2 col-xs-3 number">5</h3>
-			<div class="col-md-2 col-sm-4 col-xs-9 scope">
-				<h3 class="">SECURE</h3>
-				<p>In love yet?</p>
-			</div>
-		</div>
-		<div class="col-md-9 col-sm-12 col-xs-12 slide-right">
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec convallis dolor, nec viverra est. Aliquam porttitor purus a dolor elementum convallis. Nulla non fermentum tortor. Nulla in ante in nulla pulvinar blandit.</p>
-			<p>Quisque mollis odio non quam vulputate, nec convallis tortor suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed elementum, metus ut vehicula commodo, quam diam accumsan odio, vel efficitur sapien purus a augue. Sed vitae purus eros.</p>
-		</div>
-	</div>
-</div>
-<div class="wrapper wrapper-6 light-pink colours">
-<div class="container client-con6">
-	<div class="row client-row6">
-		<h5 class="col-md-6 offset-md-3">Find out about the process all your candidates will go through...</h5>
-		<a class="btn col-md-3" href="services/">FIND OUT MORE</a>
-	</div>
-</div>
-</div>
-<div class="wrapper wrapper-7 pink colours">
-<div class="container client-con7">
-	<div class="row client-row7">
-		<h5 class="col-md-6 offset-md-3">Find out about the process all your candidates will go through...</h5>
-		<a class="btn col-md-3" href="services/">FIND OUT MORE</a>
-	</div>
-</div>
-</div>
+	<?php } ?>
