5fe69c2200e86b653cdc6824d09c01260a4b0e30
growoffs <f.bongo@growoffshore.com>
2017-10-24 19:51:24 +0800

    services parent template dynamic content

--- template-parts/services-content.php
+++ template-parts/services-content.php
@@ -1,91 +1,47 @@
 <div class="wrapper service-wrapper">
-	<div class="col-md-3 irc-col1 colours yellow-green">
-		<h2>IRC Profile</h2>
-		<p>Best for easier fit roles</p>
+	<?php
+	$args = array(
+	    'post_type'      => 'page',
+	    'posts_per_page' => -1,
+	    'post_parent'    => get_the_ID(),
+	    'orderby'        => 'menu_order'
+	 );
 
-		<div class="col-md-12">
-			
-			<ul class="irc-profile">
-				<li><i class="fa fa-check" aria-hidden="true"></i> IRC database search </li>
-				<li><i class="fa fa-check" aria-hidden="true"></i> Quality candidates  </li>
-				<li><i class="fa fa-check" aria-hidden="true"></i> Telephone Interviews </li>
-				<li><i class="fa fa-check" aria-hidden="true"></i> Management of offer, hire and negotiation process </li>
-			</ul>
 
-		</div>
+	$parent = new WP_Query( $args );
 
-		<div class="info-links">
-			<a class="btn" href="#">SIGN UP</a>
-			<a class="btn" href="../irc-profile/">FIND OUT MORE</a>
-		</div>
-	</div>
+	if ( $parent->have_posts() ) : ?>
 
-	<div class="col-md-3 irc-col2 colours brown-green">
-		<h2>IRC Discover</h2>
-		<p>For when you need to look a little further</p>
+	    <?php while ( $parent->have_posts() ) : $parent->the_post(); ?>
+					<?php
+					$background_color = get_field('background_color',get_the_ID());
+					$list_description = get_field('list_description',get_the_ID());
+					$list_contents = get_field('list_contents',get_the_ID());
+					?>
+					<div class="col-md-3 irc-col1 colours <?= $background_color ?>">
+						<h2><?php the_title(); ?></h2>
+						<?= $list_description ?>
 
-		<div class="col-md-12">
-			
-			<ul class="irc-profile">
-				<li><i class="fa fa-check" aria-hidden="true"></i> All of PROFILE plus: </li>
-				<li><i class="fa fa-check" aria-hidden="true"></i> Access to IRC's database of 20k+ candidates  </li>
-				<li><i class="fa fa-check" aria-hidden="true"></i> Email campaign </li>
-				<li><i class="fa fa-check" aria-hidden="true"></i> Resignation coaching </li>
-				<li><i class="fa fa-check" aria-hidden="true"></i> Management of interview and assessment process </li>
-			</ul>
+						<div class="col-md-12">
+							<?php if($list_contents) :?>
+							<ul class="irc-profile">
+								<?php foreach ($list_contents as $key => $list) { ?>
+									<li><i class="fa fa-check" aria-hidden="true"></i><?= $list['content'] ?></li>
+								<?php } ?>
+							</ul>
+							<?php endif;?>
+						</div>
 
-		</div>
+						<div class="info-links">
+							<a class="btn" href="#">SIGN UP</a>
+							<a class="btn" href="<?php the_permalink(); ?>">FIND OUT MORE</a>
+						</div>
+					</div>
 
-		<div class="info-links">
-			<a class="btn" href="#">SIGN UP</a>
-			<a class="btn" href="../irc-discover/">FIND OUT MORE</a>
-		</div>
-	</div>
+	    <?php endwhile; ?>
 
-	<div class="col-md-3 irc-col3 colours green">
-		<h2>IRC Engage</h2>
-		<p>Great for mission-critical or hard to fill roles.</p>
+	<?php endif; wp_reset_query(); ?>
 
-		<div class="col-md-12">
-			
-			<ul class="irc-profile">
-				<li><i class="fa fa-check" aria-hidden="true"> All of DISCOVER plus: </i></li>
-				<li><i class="fa fa-check" aria-hidden="true"> Vacancy analysis and strategy workshop </i></li>
-				<li><i class="fa fa-check" aria-hidden="true"> Video Jobcasts </i></li>
-				<li><i class="fa fa-check" aria-hidden="true"> Targeted PPC campaign </i></li>
-				<li><i class="fa fa-check" aria-hidden="true"> face to face interviews </i></li>
-				<li><i class="fa fa-check" aria-hidden="true"> Risk and share scheme </i></li>
-			</ul>
-
-		</div>
-
-		<div class="info-links">
-			<a class="btn" href="#">SIGN UP</a>
-			<a class="btn" href="../irc-engage/">FIND OUT MORE</a>
-		</div>
-
-	</div>
-
-	<div class="col-md-3 irc-col4 colours avocado-green">
-		<h2>IRC Talent Partner</h2>
-		<p>A truly comprehensive service.</p>
-
-		<div class="col-md-12">
-			
-			<ul class="irc-profile">
-				<li><i class="fa fa-check" aria-hidden="true"> All of engage plus: </i></li>
-				<li><i class="fa fa-check" aria-hidden="true"> Your own dedicated IRC resources </i></li>
-				<li><i class="fa fa-check" aria-hidden="true"> Full access to all of IRC's tools and services </i></li>
-				<li><i class="fa fa-check" aria-hidden="true"> All of IRC's proven experoennce and expertise at your disposal </i></li>
-			</ul>
-
-		</div>
-
-		<div class="info-links">
-			<a class="btn" href="#">SIGN UP</a>
-			<a class="btn" href="../irc-talent-partner/">FIND OUT MORE</a>
-		</div>
-	</div>
 
 <div class="col-md-12 colours blue-green info-row">
 	<div class="col-md-4 ">
@@ -94,8 +50,7 @@
 	<div class="col-md-8">
 		<p>Recruiting sales people is sometimes painful and you shouldn't worry as we can help make it  a lot easier. The difference between getting it wrong or right is often the difference between your success and failure in your own role. Our proven consistent methodlogy coupled with our comfort in sharing the risk of hiring with you means you can be fully assured. Call us to find out how we can making sales hiring easier for you.</p>
 	</div>
-	
+
 </div>
 
-</div>	
-
+</div>
