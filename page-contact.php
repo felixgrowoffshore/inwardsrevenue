89fa3a46abc6b542f6e217ca391ead3bdb46c6d9
markozdecuzcuz <gaudianomark@gmail.com>
2017-10-24 16:48:29 +0800

    contact page

---
+++ page-contact.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: Contact us Page
+ */
+
+?>
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<div class="feat-wrap">
+				<?php get_template_part( 'template-parts/_featured_image' ); ?>
+			</div>
+			<div class="contact-content">
+				<?php get_template_part( 'template-parts/contact','content' ); ?>
+			</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+
+<?php
+//get_sidebar();
+get_footer();
+?>
