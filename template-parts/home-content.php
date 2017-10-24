e99b5da8290586c117fa9ce59143b13a579f0ae2
growoffs <f.bongo@growoffshore.com>
2017-10-24 14:59:28 +0800

    add jobs styling

--- template-parts/home-content.php
+++ template-parts/home-content.php
@@ -17,7 +17,7 @@
 
 	</div>
 </div>
-<div class="col-md-12 second-content">
+<div class="second-content">
 	<div class="row">
 		<div class="col-md-6 col-sm-6 col-xs-12 first-col">
 			<h2>CLIENTS</h2>
@@ -31,99 +31,14 @@
 		</div>
 	</div>
 </div>
-<div class="container col-md-12 third-content">
+<div class="container third-content">
 	<div class="row">
 		<div class="col-md-12 third-col">
 			<h2>Currently recruiting for</h2>
 			<p>Here a just few of the job roles we are currently recruiting for.</p>
 		</div>
-		<div id="field" class="container">
-			<div class="row">
-				<div class="col-md-12 input-field">
-					<div id="fourth-content">
-						<input class="col-md-5" type="input" name="input" placeholder="KEYWORDS">
-						<input class="col-md-5" type="input" name="input" placeholder="LOCATION">
-						<a href="" class="btn col-md-2">SEARCH</a>
-  					<div id="filter">
-  						<p class="">Filter by tag :
-  						<a href="" class="btn">tag title</a>
-  						<a href="" class="btn">tag title</a>
-  						<a href="" class="btn">tag title</a>
-  						<a href="" class="btn">tag title</a></p>
-  					</div>
-					</div>
-				</div>
-			</div>
+		<div class="col-md-12">
+			<?php echo do_shortcode('[jobs per_page="12"]');?>
 		</div>
 	</div>
-</div>
-<div class="container fifth-content">
-  <div class="row fifth-row">
-	<div class="col-md-12">
-		<form>
-      <div class="checkbox checkbox-inline checkbox-primary">
-          <input id="checkbox1" class="styled" type="checkbox">
-          <label for="checkbox1">
-              FREELANCE
-          </label>
-      </div>
-
-      <div class="checkbox checkbox-inline checkbox-primary">
-          <input id="checkbox2" class="styled" type="checkbox">
-          <label for="checkbox2">
-              FULL TIME
-          </label>
-      </div>
-
-      <div class="checkbox checkbox-inline checkbox-primary">
-          <input id="checkbox3" class="styled" type="checkbox">
-          <label for="checkbox3">
-              PART TIME
-          </label>
-      </div>
-
-      <div class="checkbox checkbox-inline checkbox-primary">
-          <input id="checkbox4" class="styled" type="checkbox">
-          <label for="checkbox4">
-              TEMPORARY
-          </label>
-      </div>
-
-		</form>
-  </div>
-	</div>
-
-</div>
-<div class="container sixth-content">
-	<div class="row sixth-row">
-		<div class="col-md-1">
-			<img src="<?= IMAGEPATH.'placeholder.png' ?>">
-		</div>
-		<div class="col-md-5">
-			<h5>Job Title</h5>
-			<p>Lorem ipsum dolor sit amet.</p>
-		</div>
-		<div class="col-md-3 london">
-			<p>London</p>
-		</div>
-		<div class="col-md-3 view-job">
-			<a class="btn" href="">VIEW JOB</a>
-		</div>
-	</div>
-  <div class="row sixth-row">
-		<div class="col-md-1">
-			<img src="<?= IMAGEPATH.'placeholder.png' ?>">
-		</div>
-		<div class="col-md-5">
-			<h5>Job Title</h5>
-			<p>Lorem ipsum dolor sit amet.</p>
-		</div>
-		<div class="col-md-3 london">
-			<p>London</p>
-		</div>
-		<div class="col-md-3 view-job">
-			<a class="btn" href="">VIEW JOB</a>
-		</div>
-	</div>
-
 </div>
