85178b2df2ea547e85ee4a67e08d81795f9f6622
growoffs <f.bongo@growoffshore.com>
2017-10-25 18:49:16 +0800

    services adjust styles

--- template-parts/services-content.php
+++ template-parts/services-content.php
@@ -1,4 +1,4 @@
-<div class="wrapper service-wrapper">
+<div class="service-wrapper service-flex row">
 	<?php
 	$args = array(
 	    'post_type'      => 'page',
@@ -18,11 +18,11 @@
 					$list_description = get_field('list_description',get_the_ID());
 					$list_contents = get_field('list_contents',get_the_ID());
 					?>
-					<div class="col-md-3 irc-col1 colours <?= $background_color ?>">
+					<div class="col-md-3 col-sm-6 col-xs-12 irc-col colours <?= $background_color ?>">
 						<h2><?php the_title(); ?></h2>
 						<?= $list_description ?>
 
-						<div class="col-md-12">
+						<div class="">
 							<?php if($list_contents) :?>
 							<ul class="irc-profile">
 								<?php foreach ($list_contents as $key => $list) { ?>
@@ -32,7 +32,7 @@
 							<?php endif;?>
 						</div>
 
-						<div class="info-links">
+						<div class="service-links">
 							<a class="btn" href="#">SIGN UP</a>
 							<a class="btn" href="<?php the_permalink(); ?>">FIND OUT MORE</a>
 						</div>
@@ -42,15 +42,15 @@
 
 	<?php endif; wp_reset_query(); ?>
 
-
-<div class="col-md-12 colours blue-green info-row">
-	<div class="col-md-4 ">
-		<h2>ACTIONS NOT WORDS</h2>
-	</div>
-	<div class="col-md-8">
-		<p>Recruiting sales people is sometimes painful and you shouldn't worry as we can help make it  a lot easier. The difference between getting it wrong or right is often the difference between your success and failure in your own role. Our proven consistent methodlogy coupled with our comfort in sharing the risk of hiring with you means you can be fully assured. Call us to find out how we can making sales hiring easier for you.</p>
-	</div>
-
 </div>
+<div class="service-wrapper row">
+	<div class="col-md-12 colours blue-green info-row">
+		<div class="col-md-4 ">
+			<h2>ACTIONS NOT WORDS</h2>
+		</div>
+		<div class="col-md-8">
+			<p>Recruiting sales people is sometimes painful and you shouldn't worry as we can help make it  a lot easier. The difference between getting it wrong or right is often the difference between your success and failure in your own role. Our proven consistent methodlogy coupled with our comfort in sharing the risk of hiring with you means you can be fully assured. Call us to find out how we can making sales hiring easier for you.</p>
+		</div>
 
+	</div>
 </div>
