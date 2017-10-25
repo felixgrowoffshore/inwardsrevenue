dd6a452cb12e60fc8040b0b9b1292efceec618b5
growoffs <f.bongo@growoffshore.com>
2017-10-25 15:08:17 +0800

    add script for captcha

--- library/development/js/theme-scripts.js
+++ library/development/js/theme-scripts.js
@@ -117,14 +117,28 @@ jQuery(document).ready(function($){
     $(".num2").text(b);
 
     var ans_c = a + b;
+
     // console.log(ans_c);
-    $("#IRC-from").submit(function(){
+    $("#IRC-from form").submit(function(){
       var ans = $(".answer").val();
       var flag = true;
       if(ans_c != ans) flag = false;
       if(!flag) $(".wpcf7-validation-errors").append("Incorrect captcha");
 
       return flag;
+    });
+
+    $(".btn-submit").click(function(){
+      var ans = $(".answer").val();
+      var flag = true;
+      if(ans_c != ans) flag = false;
+
+      if(flag){
+        $("#IRC-from form").trigger("submit");
+      } else {
+        $(".wpcf7-response-output").addClass("wpcf7-validation-errors").removeClass("wpcf7-mail-sent-ok wpcf7-display-none").html("Incorrect captcha");
+      }
+      // return flag;
     });
   }
 
